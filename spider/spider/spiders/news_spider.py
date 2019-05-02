6b7aa7171f70e074ba20c5195059e28f15535ea8
pipecat <pipecat@yahoo.com>
2019-05-02 16:37:13 +0800

    add a new spider

--- spider/spider/spiders/news_spider.py
+++ spider/spider/spiders/news_spider.py
@@ -90,3 +90,37 @@ class CurrentNewsSpider(scrapy.Spider):
             'news_time': response.css('.news_about p::text')[1].get().strip(),
             'news_txt': ''.join(response.css('.news_txt::text').getall())
         }
+
+
+'''load_chosen.jsp?nodeids=25949&topCids=3385400,3388…3388463,3368844,&pageidx=5&lastTime=1556751771400
+'''
+
+class NewsSpider(scrapy.Spider):
+    name = "news"
+
+    def start_requests(self):
+        urls = []
+        for i in range(1,25):
+            urls.append("https://www.thepaper.cn/load_chosen.jsp?nodeids=25949&topCids=3385400,3388…3388463,3368844,&pageidx=" + str(i) +"&lastTime=1556751771400")
+        custom_settings = {
+            'USER-AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+        }
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        links = response.css('.news_tu a::attr(href)').getall()
+        urls = []
+        for link in links:
+            url = "https://www.thepaper.cn/" + link
+            urls.append(url)
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse_detail)
+
+    def parse_detail(self, response):
+        yield {
+            'news_title': response.css('.news_title::text').get(),
+            'news_source': response.css('.news_about p::text')[0].get(),
+            'news_time': response.css('.news_about p::text')[1].get().strip(),
+            'news_txt': ''.join(response.css('.news_txt::text').getall())
+        }
